362a535f5d790a470a84d7ef2d23e744c368edad
mxdi9i7 <peter.zheng88228@gmail.com>
2018-08-13 00:25:18 -0400

    fixed click bug on phone and resp[onsive top container, added new favicon

--- src/components/CoverArticle.js
+++ src/components/CoverArticle.js
@@ -10,6 +10,7 @@ class CoverArticle extends Component {
     return (
       <div 
         onMouseOver={() => this.props.handleHover(this.props.index)}
+        onClick={() => this.props.handleHover(this.props.index)}
         className={classList.join(' ')}>
         <h2>0{this.props.index + 1}</h2>
         {
