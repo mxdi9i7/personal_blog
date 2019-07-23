581e26787db49300f347a40fa81415dc16d4b1d4
mxdi9i7 <peter.zheng88228@gmail.com>
2019-07-23 08:28:40 -0400

    hide token

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import { setContext } from "apollo-link-context";
 import { InMemoryCache } from "apollo-cache-inmemory";
 import { ApolloProvider } from "react-apollo";
 
-const token = "61953a1542da2d4cd8ecb0e8e32cfa";
+const token = process.env.REACT_APP_DATO_TOKEN;
 
 const httpLink = createHttpLink({
   uri: "https://graphql.datocms.com/"
